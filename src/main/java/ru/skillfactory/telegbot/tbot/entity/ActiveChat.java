b99819f84ca6d12151ac498838c4da55090ea1b6
Michael Kirss <Michaelkirss@gmail.com>
2024-01-15 20:31:41 +0400

    save no Shedule

--- src/main/java/ru/skillfactory/telegbot/tbot/entity/ActiveChat.java
+++ src/main/java/ru/skillfactory/telegbot/tbot/entity/ActiveChat.java
@@ -4,17 +4,14 @@ import lombok.Data;
 
 import javax.persistence.*;
 
-@Data  //Для того, чтобы не писать геттеры/сеттеры
-@Entity // Данный класс является JPA сущностью
-@Table(name = "ACTIVE_CHAT") //И хранится в таблице ACTIVE_CHAT
+@Data
+@Entity
+@Table(name = "ACTIVE_CHAT")
 public class ActiveChat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="CHAT_ID")
+    private Long chatId;
 
-    public class ActiveChat {
-
-        @Id
-        @GeneratedValue
-        private Long id; //Уникальный идентификатор в системе нашего бота
-
-        @Column(name = "CHAT_ID")
-        private Long chatId; //Уникальный идентификатор в системе Telegram
 }
--- src/main/java/ru/skillfactory/telegbot/tbot/service/BotService.java
+++ src/main/java/ru/skillfactory/telegbot/tbot/service/BotService.java
@@ -13,10 +13,12 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.skillfactory.telegbot.tbot.dto.ValuteCursOnDate;
+import ru.skillfactory.telegbot.tbot.entity.ActiveChat;
+import ru.skillfactory.telegbot.tbot.repository.ActiveChatRepository;
+
 
 import javax.annotation.PostConstruct;
-
-
+import java.util.Set;
 
 
 @Service //Данный класс является сервисом
@@ -24,48 +26,21 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 public class BotService extends TelegramLongPollingBot{
 
-
     private final CentralRussianBankService centralBankRussianService;
+    private final ActiveChatRepository activeChatRepository;
 
     @Value("${bot.api.key}") //Сюда будет вставлено значение из application.properties, в котором будет указан api key, полученный от BotFather
     private String apiKey;
 
     @Value("${bot.name}") //Как будут звать нашего бота
     private String name;
-
     //Это основной метод, который связан с обработкой сообщений
-    @Override
-    public void onUpdateReceived(Update update) {
-        Message message = update.getMessage(); //Этой строчкой мы получаем сообщение от пользователя
-        try {
-            SendMessage response = new SendMessage(); //Данный класс представляет собой реализацию команды отправки сообщения, которую за нас выполнит ранее подключенная библиотека
-            Long chatId = message.getChatId(); //ID чата, в который необходимо отправить ответ
-            response.setChatId(String.valueOf(chatId)); //Устанавливаем ID, полученный из предыдущего этап сюда, чтобы сообщить, в какой чат необходимо отправить сообщение
 
-            //Тут начинается самое интересное - мы сравниваем, что прислал пользователь, и какие команды мы можем обработать. Пока что у нас только одна команда
-            if ("/currentrates".equalsIgnoreCase(message.getText())) {
-//Получаем все курсы валют на текущий момент и проходимся по ним в цикле         
-                for (ValuteCursOnDate valuteCursOnDate : centralBankRussianService.getCurrenciesFromCbr()) {
-//В данной строчке мы собираем наше текстовое сообщение
-//StringUtils.defaultBlank – это метод из библиотеки Apache Commons, который нам нужен для того, чтобы на первой итерации нашего цикла была вставлена пустая строка вместо null, а на следующих итерациях не перетерся текст, полученный из предыдущих итерации. Подключение библиотеки см. ниже
-                    response.setText(StringUtils.defaultIfBlank(response.getText(), "") + valuteCursOnDate.getName() + " - " + valuteCursOnDate.getCourse() + "\n");
-                }
-            }
-            //Теперь мы сообщаем, что пора бы и ответ отправлять
-            execute(response);
-            //Ниже очень примитивная обработка исключений, чуть позже мы это поправим
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     //Данный метод будет вызван сразу после того, как данный бин будет создан - это обеспечено аннотацией Spring PostConstruct
     @PostConstruct
     public void start() {
         log.info("username: {}, token: {}", name, apiKey);
-
     }
 
     //Данный метод просто возвращает данные о имени бота и его необходимо переопределять
@@ -77,5 +52,51 @@ public class BotService extends TelegramLongPollingBot{
     @Override
     public String getBotToken() {
         return apiKey;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage(); //Этой строчкой мы получаем сообщение от пользователя
+        try {
+            Long chatId = message.getChatId();
+            SendMessage response = new SendMessage(); //Данный класс представляет собой реализацию команды отправки сообщения, которую за нас выполнит ранее подключенная библиотека
+                                    //ID чата, в который необходимо отправить ответ
+            response.setChatId(String.valueOf(chatId));
+
+            //Устанавливаем ID, полученный из предыдущего этап сюда, чтобы сообщить, в какой чат необходимо отправить сообщение
+            //Тут начинается самое интересное - мы сравниваем, что прислал пользователь, и какие команды мы можем обработать. Пока что у нас только одна команда
+            if ("/currentrates".equalsIgnoreCase(message.getText())) {
+//Получаем все курсы валют на текущий момент и проходимся по ним в цикле
+                for (ValuteCursOnDate valuteCursOnDate : centralBankRussianService.getCurrenciesFromCbr()) {
+//В данной строчке мы собираем наше текстовое сообщение
+//StringUtils.defaultBlank – это метод из библиотеки Apache Commons, который нам нужен для того, чтобы на первой итерации нашего цикла была вставлена пустая строка вместо null, а на следующих итерациях не перетерся текст, полученный из предыдущих итерации. Подключение библиотеки см. ниже
+                    response.setText(StringUtils.defaultIfBlank(response.getText(), "") + valuteCursOnDate.getName() + " - " + valuteCursOnDate.getCourse() + "\n");
+                }
+                execute(response);
+            }
+
+            if (activeChatRepository.findActiveChatByChatId(chatId).isEmpty()){
+                ActiveChat activeChat = new ActiveChat();
+                activeChat.setChatId(chatId);
+                activeChatRepository.save(activeChat);
+            }
+            //Ниже очень примитивная обработка исключений, чуть позже мы это поправим
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void sendNotificationToAllActiveChats(String message, Set<Long> chatIds) {
+        for (Long id : chatIds) {
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(String.valueOf(id));
+            sendMessage.setText(message);
+            try {
+                execute(sendMessage);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
